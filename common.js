264cdc363c6927afa81f9f5b6729e637fbebb3be
Pipe <yys320carter@163.com>
2016-01-15 15:39:33 +0800

    Update common.js

--- common.js
+++ common.js
@@ -3,3 +3,12 @@ function getQueryString(name) {
     var r = window.location.search.substr(1).match(reg);
     if (r != null) return decodeURI(r[2]); return null;
 }
+
+String.prototype.format = function() {
+    for (var t = this,
+    e = 0; e < arguments.length; e++) {
+        var n = new RegExp("\\{" + e + "\\}", "gi");
+        t = t.replace(n, arguments[e])
+    }
+    return t
+}
